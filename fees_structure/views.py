dc548c0bacc9a3716f0c5535bc434a3aa356a678
Gabobe15 <gabobe33@gmail.com>
2024-02-01 23:14:51 +0300

    studentFee view and url

--- fees_structure/views.py
+++ fees_structure/views.py
@@ -5,6 +5,8 @@ from rest_framework.pagination import PageNumberPagination
 from rest_framework import filters, permissions
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.response import Response
+from rest_framework.exceptions import AuthenticationFailed
+from datetime import datetime,timedelta
 # generics
 from rest_framework import  permissions, generics
 
@@ -84,4 +86,29 @@ class AddFees(generics.GenericAPIView):
 #     ordering_fields = ['regno','fees', 'payment', 'balance', 'is_active']
   
 
-    
+    # Single student fees
+class StudentFeesView(generics.ListAPIView):
+    permission_classes = [ permissions.AllowAny ]
+    serializer_class = FeesStructureSerializer
+    pagination_class = CustomPageNumberPagination
+
+    def get_queryset(self):
+        student = self.kwargs['student']
+
+        fees = FeesStructure.objects.filter(student=student, is_active=True).order_by('-id')
+
+        if not fees:
+            raise AuthenticationFailed('Student not found!')
+
+        # Date Filters
+        from_date = self.request.query_params.get('from_date',None)
+        to_date = self.request.query_params.get('to_date',None)
+
+        if from_date and to_date:
+            date_format='%d-%m-%Y'
+            from_date=datetime.strptime(from_date, date_format) #Convert string into date format
+            to_date=datetime.strptime(to_date,date_format)
+            to_date=to_date+timedelta(days=1) # add extra day in date search
+            fees=fees.filter(payment_date__range=[from_date,to_date]) 
+
+        return fees
